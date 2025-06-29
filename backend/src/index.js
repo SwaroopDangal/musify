af6ff937e216043ca85736821c92f92e0f0c9fac
SwaroopDangal <swaroopdangal732@gmail.com>
2025-06-29 11:23:31 +0545

    fix: replace wildcard route to support Express 5 compatibility

--- backend/src/index.js
+++ backend/src/index.js
@@ -53,7 +53,7 @@ app.use(
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
+  app.get("/:any*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   });
 }
